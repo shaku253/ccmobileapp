5f5f8f05bf263974c62c1d7d1d45f78d63719354
shaku253 <shaku253@yahoo.fr>
2017-04-21 11:19:40 +0200

    Initial Commit

---
+++ js/my-app.js
@@ -0,0 +1,30 @@
+// Init App
+var myApp = new Framework7({
+    modalTitle: 'Framework7',
+    // Enable Material theme
+    material: true
+});
+
+// Expose Internal DOM library
+var $$ = Dom7;
+
+$$('a').on('click', function (e) { //Close panel when you open a new page
+    myApp.closePanel();
+});
+
+// Add main view
+var mainView = myApp.addView('.view-main', {
+});
+
+myApp.onPageInit('cart', function (page) {
+      $$('.delete').on('click', function(){
+        myApp.swipeoutDelete($$('li.swipeout.product1'));
+      });
+      $$('.delete2').on('click', function(){
+        myApp.swipeoutDelete($$('li.swipeout.product2'));
+      });
+      $$('.delete3').on('click', function(){
+        myApp.swipeoutDelete($$('li.swipeout.product3'));
+      });
+});
+
