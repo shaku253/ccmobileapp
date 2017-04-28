40e80fcf45c67d12b631fec00933fcdddf35d53f
shaku253 <shaku253@yahoo.fr>
2017-04-28 19:12:28 +0200

    icon and splash screen WIP

--- js/my-app.js
+++ js/my-app.js
@@ -3,7 +3,35 @@ var myApp = new Framework7({
     modalTitle: 'Framework7',
     // Enable Material theme
     material: true
+    var options = {
+  'bgcolor': '#0da6ec',
+  'fontcolor': '#fff'
 });
+var welcomescreen_slides = [
+  {
+    id: 'slide0',
+    picture: '<div class="tutorialicon">♥</div>',
+    text: 'Welcome to this tutorial. In the next steps we will guide you through a manual that will teach you how to use this app.'
+  },
+  {
+    id: 'slide1',
+    picture: '<div class="tutorialicon">✲</div>',
+    text: 'This is slide 2'
+  },
+  {
+    id: 'slide2',
+    picture: '<div class="tutorialicon">♫</div>',
+    text: 'This is slide 3'
+  },
+  {
+    id: 'slide3',
+    picture: '<div class="tutorialicon">☆</div>',
+    text: 'Thanks for reading! Enjoy this app.<br><br><a id="tutorial-close-btn" href="#">End Tutorial</a>'
+  }
+];
+var welcomescreen = myApp.welcomescreen(welcomescreen_slides, options);
+welcomescreen.open();  
+welcomescreen.setTimeout(welcomescreen.open(), 3000);
 
 // Expose Internal DOM library
 var $$ = Dom7;
@@ -14,6 +42,7 @@ $$('a').on('click', function (e) { //Close panel when you open a new page
 
 // Add main view
 var mainView = myApp.addView('.view-main', {
+domCache : true,
 });
 
 myApp.onPageInit('cart', function (page) {
@@ -27,4 +56,6 @@ myApp.onPageInit('cart', function (page) {
         myApp.swipeoutDelete($$('li.swipeout.product3'));
       });
 });
+
+
 
